e80ab0e6777ee3673424ff978494ff59eee4c1a0
advaitare7 <98133154+advaitare7@users.noreply.github.com>
2024-03-22 17:24:56 +0000

    datatypes

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "cr7@cr7.com"
+var accountPwd = "12345"
+accountCity = "Jaipur"
+
+let accountState
+
+// accountId = 7 // not allowed
+
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,5 @@
+"use strict"; // treat code as newer version of JS
+
+// alert("Hello") // we are using node js not browser
+
+console.log(3+3);
