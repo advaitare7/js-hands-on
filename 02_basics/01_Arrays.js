1d3ef8fe93911d2028ab5246bd82ce7112484a95
advaitare7 <98133154+advaitare7@users.noreply.github.com>
2024-03-29 05:44:27 +0000

    Something about arrays

---
+++ 02_basics/01_Arrays.js
@@ -0,0 +1,17 @@
+//  Arrays -> objects
+// copys in arrays are shallow copies -> who have the same reference types
+
+const myArray = [0,1,2,3,4,5]
+// console.log(myArray[4]);
+
+// methods
+
+// myArray.push(7);
+// myArray.push(17);
+// myArray.pop();
+
+//  unshift, shift, add remove from left
+
+// slice and splice
+// splice manipulates original array
+console.log(myArray);
