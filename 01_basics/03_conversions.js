2d662cd0930082a545f54301b5627f98795d9a30
advaitare7 <98133154+advaitare7@users.noreply.github.com>
2024-03-29 00:27:37 +0000

    Comaprisons and some bs

--- 01_basics/03_conversions.js
+++ 01_basics/03_conversions.js
@@ -12,11 +12,41 @@ let score = 'lol'
 // true => 1
 // false => 0
 
-let isLoggedIn = 'advait';
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 'advait';
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 //  1 => true
 //  0 => false
 //  " " => false
 //  'adva' => true
+// =========================OPERATIONS==============================
+
+let value = 4
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str1 = 'hello'
+// let str2 = ' cris'
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log('1' + 2);
+// console.log(1 + '2');
+// console.log(1 + '2');
+// console.log('1' + 2 + 2);
+// console.log(1 + 2 + '2');
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter =100;
+gameCounter++;
+console.log(gameCounter);
