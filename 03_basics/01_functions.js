7736689d21ff424e5a55784a547c2b8d6ef55081
advaitare7 <98133154+advaitare7@users.noreply.github.com>
2024-03-29 20:51:35 +0000

    Functions and scopes

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,14 @@
+function addTwo(num1, num2){
+    return num1+num2
+}
+
+const res = addTwo(2,2);
+// console.log(res);
+
+
+//===================================
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(2,4,5));
