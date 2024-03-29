6560e8e93e10262e4a61f910af02edf5348f872b
advaitare7 <98133154+advaitare7@users.noreply.github.com>
2024-03-29 05:24:39 +0000

    dates and what not baiscs

---
+++ 01_basics/07_Dates.js
@@ -0,0 +1,7 @@
+let date = new Date()
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toLocaleString());
+
+let newDate = new Date(2024, 18, 23)
+console.log(newDate.toDateString());
