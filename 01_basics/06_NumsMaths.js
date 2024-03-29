1b1bd4a8351d654f3bea39ef27d408feb1fafb11
advaitare7 <98133154+advaitare7@users.noreply.github.com>
2024-03-29 04:59:29 +0000

    Numbers and maths

---
+++ 01_basics/06_NumsMaths.js
@@ -0,0 +1,19 @@
+const score = 400
+// console.log(score)
+
+const num = new Number(400)
+// console.log(num)
+
+// console.log(num.toString().length);
+// ===================maths========================
+
+// console.log(Math);
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.round(4.4));
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min + 1))+ min)
