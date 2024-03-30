86667fdb5be907f1e9de2adc2ecd6cd53255e885
advaitare7 <98133154+advaitare7@users.noreply.github.com>
2024-03-30 02:38:38 +0000

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,11 +1,25 @@
-
+let a = 300;
 
 if(true){
     let a = 10
     const b = 20
     var c = 30
+    console.log(a);
 }
 
 console.log(a);
-console.log(b);
-console.log(c);
+// console.log(b);
+// console.log(c);
+
+function one(){
+    const user = "hitesh"
+
+    function two(){
+        const web = "yt.com"
+        console.log(user);
+    }
+    console.log(web);
+    two();
+}
+
+one();
